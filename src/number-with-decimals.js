7cead8d4c66aaf8c0f7cedc4bae89ef0b5f0f52b
nelynely <nelly.nf54@gmail.com>
2025-06-16 19:25:29 -0300

    Create number-with-decimals.js

---
+++ src/number-with-decimals.js
@@ -0,0 +1,9 @@
+/**
+ * Floating Precision
+ * Floating point arithmetic is not always 100% accurate
+ */
+
+const precision = 100;
+const num1 = 0.1 * precision;
+const num2 = 0.2 * precision;
+console.log((num1 + num2) / 100);
