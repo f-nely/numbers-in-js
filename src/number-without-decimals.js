0cc9b0f969bdbc44de074835d38afe8760505038
nelynely <nelly.nf54@gmail.com>
2025-06-16 19:24:46 -0300

    Create number-without-decimals.js

---
+++ src/number-without-decimals.js
@@ -0,0 +1,9 @@
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+
+/**
+ * Integer Precision
+ * Integers (numbers without a period or exponent notation) are accurate up to 15 digits
+ */
+
+const n1 = 999999999999999; // n1 will be 999999999999999
+console.log(n1);
