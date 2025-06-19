398d31fb72e580506e53e0e93adee99653c3f369
nelynely <nelly.nf54@gmail.com>
2025-06-19 11:13:02 -0300

    Create tostring.js

---
+++ src/tostring.js
@@ -0,0 +1,8 @@
+const numberToString = 3030;
+console.log(numberToString.toString());
+console.log(typeof numberToString.toString());
+
+// Converting a number to a string with base 2
+console.log(numberToString.toString(2));
+// Converting a number to a string with base 16
+console.log(numberToString.toString(16));
