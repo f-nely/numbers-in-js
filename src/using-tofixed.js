7321ae3b23080a9489b1a40edd58270181701d76
nelynely <nelly.nf54@gmail.com>
2025-06-19 11:11:54 -0300

    Create using-tofixed.js

---
+++ src/using-tofixed.js
@@ -0,0 +1,6 @@
+const fuelPrice = 4.368;
+const result = fuelPrice * 100;
+const valuePerWeek = fuelPrice * 100 * 52;
+
+console.log(valuePerWeek.toFixed()); // 22714
+console.log(valuePerWeek.toFixed(2)); // 22713.60
